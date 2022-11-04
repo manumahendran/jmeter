1a5a4ea2b87a3c149925928221ad66d08fe0e4d3
Manu Mahendran <manu.mahendran@pwc.com>
2022-11-04 09:16:38 +0530

    Basic REST Api Code

---
+++ src/main/java/com/ubs/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.ubs.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController{
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Spring Boot!";
+    }
+
+
+}
