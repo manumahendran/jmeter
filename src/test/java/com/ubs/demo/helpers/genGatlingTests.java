a3bdf9ce6c42aca92b557d8397290ae3d030d7fc
Manu Mahendran <manu.mahendran@pwc.com>
2022-11-15 21:35:47 +0530

    implemented sgatling tests generator

---
+++ src/test/java/com/ubs/demo/helpers/genGatlingTests.java
@@ -0,0 +1,135 @@
+package com.ubs.demo.helpers;
+
+import com.google.common.collect.Maps;
+import com.hubspot.jinjava.Jinjava;
+import io.swagger.models.*;
+import io.swagger.models.parameters.BodyParameter;
+import io.swagger.models.parameters.Parameter;
+import io.swagger.models.parameters.PathParameter;
+import io.swagger.models.properties.ArrayProperty;
+import io.swagger.models.properties.Property;
+import io.swagger.models.properties.RefProperty;
+import io.swagger.models.refs.RefFormat;
+import io.swagger.parser.SwaggerParser;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
+public class genGatlingTests {
+    static Map<String, Object> context = Maps.newHashMap();
+    static Map<String, String> properties = Maps.newHashMap();
+    static Map<String, String> pathParmeters = Maps.newHashMap();
+
+    static String fileName = "src/test/resources/gatlingTestTemplate.jinja";
+    static String template;
+
+    static {
+        try {
+            template = Files.readString(Paths.get(fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public genGatlingTests() throws IOException {
+    }
+
+    public static void main(String[] args) throws IOException {
+        Jinjava jinjava = new Jinjava();
+//        parameters.put("id","123");
+
+        Swagger swagger = new SwaggerParser().read("src/test/resources/swaggerDocs/swagger.json");
+
+        for(Map.Entry<String, Path> entry : swagger.getPaths().entrySet()) {
+            System.out.println("===Path====\n"+entry.getKey());
+            context.put("path",entry.getKey());
+            printOperations(swagger, entry.getValue().getOperationMap());
+            String renderedTemplate = jinjava.render(template, context);
+            System.out.println(renderedTemplate);
+            context.clear();
+            properties.clear();
+            pathParmeters.clear();
+        }
+    }
+
+    private static void printOperations(Swagger swagger, Map<HttpMethod, Operation> operationMap) {
+        for(Map.Entry<HttpMethod, Operation> op : operationMap.entrySet()) {
+            context.put("method",op.getKey());
+            context.put("OperationID", op.getValue().getOperationId());
+            System.out.println(op.getKey() + " - " + op.getValue().getOperationId());
+            System.out.println("Parameters:");
+            for(Parameter p : op.getValue().getParameters()) {
+                if(p instanceof BodyParameter) {
+                    printBody(swagger, (BodyParameter) p);
+                } else {
+                    String paramType = p.getClass().getSimpleName();
+                    if(p instanceof PathParameter) {
+                        paramType = "path";
+                    }
+                    System.out.println(p.getName() + " : " + paramType);
+                    pathParmeters.put(p.getName(),paramType);
+                }
+            }
+            context.put("properties", properties);
+            System.out.println();
+            printResponses(swagger, op.getValue().getResponses());
+            System.out.println();
+        }
+    }
+
+    private static void printBody(Swagger swagger, BodyParameter p) {
+        System.out.println("BODY: ");
+
+        RefProperty rp = new RefProperty(p.getSchema().getReference());
+        printReference(swagger, rp);
+    }
+
+    private static void printResponses(Swagger swagger, Map<String, Response> responseMap) {
+        System.out.println("Responses:");
+        for(Map.Entry<String, Response> response : responseMap.entrySet()) {
+            System.out.println(response.getKey() + ": " + response.getValue().getDescription());
+
+            if(response.getValue().getSchema() instanceof RefProperty) {
+                RefProperty rp = (RefProperty)response.getValue().getSchema();
+                printReference(swagger, rp);
+            }
+
+            if(response.getValue().getSchema() instanceof ArrayProperty) {
+                ArrayProperty ap = (ArrayProperty)response.getValue().getSchema();
+                if(ap.getItems() instanceof RefProperty) {
+                    RefProperty rp = (RefProperty)ap.getItems();
+                    System.out.println(rp.getSimpleRef() + "[]");
+                    printReference(swagger, rp);
+                }
+            }
+        }
+    }
+
+    private static void printReference(Swagger swagger, RefProperty rp) {
+        if(rp.getRefFormat().equals(RefFormat.INTERNAL) &&
+                swagger.getDefinitions().containsKey(rp.getSimpleRef())) {
+            Model m = swagger.getDefinitions().get(rp.getSimpleRef());
+
+            if(m instanceof ArrayModel) {
+                ArrayModel arrayModel = (ArrayModel)m;
+                System.out.println(rp.getSimpleRef() + "[]");
+                if(arrayModel.getItems() instanceof RefProperty) {
+                    RefProperty arrayModelRefProp = (RefProperty)arrayModel.getItems();
+                    printReference(swagger, arrayModelRefProp);
+                }
+            }
+
+            if(m.getProperties() != null) {
+                for (Map.Entry<String, Property> propertyEntry : m.getProperties().entrySet()) {
+                    System.out.println("  " + propertyEntry.getKey() + " : " + propertyEntry.getValue().getType());
+                    properties.put(propertyEntry.getKey(),propertyEntry.getValue().getType());
+
+                }
+            }
+        }
+    }
+
+}
+
--- src/test/java/com/ubs/demo/perfTests/apiTest.java
+++ src/test/java/com/ubs/demo/perfTests/apiTest.java
@@ -10,8 +10,7 @@ import static io.gatling.javaapi.http.HttpDsl.status;
 public class apiTest extends Simulation{
 
     //protocol
-    private HttpProtocolBuilder httpProtocol = http
-            .baseUrl("https://reqres.in/api");
+    private HttpProtocolBuilder httpProtocol = http.baseUrl("https://reqres.in/api");
 
     //sceanrio
     private ScenarioBuilder Createusers = scenario("Create User").exec(
@@ -25,30 +24,11 @@ public class apiTest extends Simulation{
 //                            "}")).asJson()
                     .check(status().is(201),jsonPath("$.name").is("qaautomationhub"))).pause(1);
 
-    private ScenarioBuilder Upateusers = scenario("Update User").exec(
-            http("Update the User")
-                    .put("/users/2").header("content-type","application/json")
-                    .asJson()
-                    .body(RawFileBody("Data/user.json"))
-//
-                    .check(status().is(200),jsonPath("$.name").is("qaautomationhub")));
 
-    private ScenarioBuilder Deleteusers = scenario("Delete User").exec(
-            http("Delete the User")
-                    .delete("/users/2").header("content-type","application/json")
-                     .check(status().is(204)));
-    private ScenarioBuilder FetchUsers = scenario("Get API Request Demo").exec(
-            http("Get Single User")
-                    .get("/users/2").check(
-                            status().is(200)).
-                    check(jsonPath("$.data.first_name").is("Janet"))
-    ).pause(1);
+
     {
         setUp(
-                Createusers.injectOpen(rampUsers(5).during(5)),
-                Upateusers.injectOpen(rampUsers(10).during(7)),
-                Deleteusers.injectOpen(rampUsers(10).during(7))).protocols(httpProtocol);
-        FetchUsers.injectOpen(atOnceUsers(30)).protocols(httpProtocol);
+                Createusers.injectOpen(rampUsers(5).during(5))).protocols(httpProtocol);
     }
 }
 
